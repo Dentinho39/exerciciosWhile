cfcd6a773e492c74c0751f2bdd3e4b183ad60414
Júlio César Da Silva <142625113+Dentinho39@users.noreply.github.com>
2026-04-13 09:05:42 -0300

    Update exc6.java

--- exc6.java
+++ exc6.java
@@ -2,8 +2,32 @@ import java.util.Scanner;
 
 //Júlio César da Silva
 
-public class exc6 {
+public class Exercicio6 {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(System.in);
+
+        int contador = 1;
+        int numero;
+        int menor;
+
+        System.out.println("Digite o 1º número: ");
+        numero = scanner.nextInt();
+        menor = numero;
+
+        while (contador < 10) {
+            System.out.printf("%nDigite o %dº número: " , contador + 1);
+            numero = scanner.nextInt();
+
+            if (numero < menor) {
+                menor = numero;
+            }
+
+            contador++;
+        }
+
+        System.out.println("O menor número é: " + menor);
+
+        scanner.close();
     }
 }
